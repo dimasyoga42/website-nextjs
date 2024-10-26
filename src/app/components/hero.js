d46d1a8da215046dc0593951011b1c1b9a901ae8
dimasyoga42 <dimasyoga9090@gmail.com>
2024-10-27 01:23:42 +0700

    add: hero

---
+++ src/app/components/hero.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const Hero = () => {
+return (
+    <>
+    <div className="hero h-screen font-fira">
+        <div className="hero-content text-center block">
+           
+            <h1 className=" text-5xl font-bold font-fira text-white">Dimasyoga</h1>
+            <p className="font- text-sm mt-1 text-gray-400">Front end Developer | Student</p>
+        </div>
+    </div>
+    </>
+)
+}
+export default Hero;
