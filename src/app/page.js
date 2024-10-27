0cd08f4a32ff280355563a50bba0b84d13e7a5dd
dimasyoga42 <dimasyoga9090@gmail.com>
2024-10-27 22:28:19 +0700

    ..

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Hero } from "./components/hero";
 import Fotter  from "./components/fotter";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Page from "./pages/page";
+import Page from "./pages/home";
 export default function Home() {
   return (
     <div className="container w-full md:w-1/2">
